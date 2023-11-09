82d8d69a899272a61cb58106ce4b0c1df8a9364c
Shre-D <shreyash1264@gmail.com>
2023-11-09 18:15:23 +0530

    configure deploy file for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://icaef-2024.github.io',
+  base: '/Economics-Conference-Website',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
